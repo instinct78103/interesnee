f56f46a63a7597a8b04ae2d096919a273b8da006
instinct78103 <instinct78103@gmail.com>
2024-10-15 14:43:33 +0500

    Add raw map component-2

--- src/components/OfficeSlider.vue
+++ src/components/OfficeSlider.vue
@@ -6,7 +6,6 @@
       :is-manual-sliding="true"
       :options="sliderOpts"
       :go-slide="currentSlider"
-      @afterChange="slideChanged"
     >
       <div
         v-for="(value, index) in office"
@@ -17,27 +16,27 @@
             :class="$style.officePhotos"
             @click="openCarousel(value)"
           >
-            <el-carousel
-              :interval="4000"
-              :autoplay="true"
-              :arrow="value.images.length > 1 ? 'always' : 'never'"
-              :indicator-position="value.images.length > 1 ? 'outside' : 'none'"
-              height="180px"
-              trigger="click"
-            >
-              <el-carousel-item
-                v-for="(image, index) in value.images"
-                :key="index"
-              >
-                <app-image
-                  :class="$style.carouselImg"
-                  :x1="image.origin"
-                  :webp="image.webp"
-                  alt="Office photo"
-                  lazy
-                />
-              </el-carousel-item>
-            </el-carousel>
+            <!--            <el-carousel-->
+            <!--              :interval="4000"-->
+            <!--              :autoplay="true"-->
+            <!--              :arrow="value.images.length > 1 ? 'always' : 'never'"-->
+            <!--              :indicator-position="value.images.length > 1 ? 'outside' : 'none'"-->
+            <!--              height="180px"-->
+            <!--              trigger="click"-->
+            <!--            >-->
+            <!--              <el-carousel-item-->
+            <!--                v-for="(image, index) in value.images"-->
+            <!--                :key="index"-->
+            <!--              >-->
+            <!--                <app-image-->
+            <!--                  :class="$style.carouselImg"-->
+            <!--                  :x1="image.origin"-->
+            <!--                  :webp="image.webp"-->
+            <!--                  alt="Office photo"-->
+            <!--                  lazy-->
+            <!--                />-->
+            <!--              </el-carousel-item>-->
+            <!--            </el-carousel>-->
           </div>
           <div :class="$style.textWrap">
             <template v-if="value.link">
@@ -58,38 +57,68 @@
     </BaseSlider>
 
     <!-- Office dialog -->
-    <el-dialog
-      :visible.sync="showDialog"
-      :class="['dialogWithCarousel', $style.dialog]"
-    >
-      <el-carousel
-        v-if="selectedOffice"
-        :interval="4000"
-        :autoplay="false"
-        :class="$style.dialogSlider"
-        :arrow="selectedOffice.images.length > 1 ? 'always' : 'never'"
-        indicator-position="none"
-        trigger="click"
-      >
-        <el-carousel-item
-          v-for="(image, index) in selectedOffice.images"
-          :key="index"
-          :class="$style.dialogSliderItem">
-          <app-image
-            :class="$style.dialogImg"
-            :x1="image.origin"
-            :webp="image.webp"
-            alt="Office photo"
-            lazy
-          />
-        </el-carousel-item>
-      </el-carousel>
-    </el-dialog>
+    <!--    <el-dialog-->
+    <!--      :visible.sync="showDialog"-->
+    <!--      :class="['dialogWithCarousel', $style.dialog]"-->
+    <!--    >-->
+    <!--      <el-carousel-->
+    <!--        v-if="selectedOffice"-->
+    <!--        :interval="4000"-->
+    <!--        :autoplay="false"-->
+    <!--        :class="$style.dialogSlider"-->
+    <!--        :arrow="selectedOffice.images.length > 1 ? 'always' : 'never'"-->
+    <!--        indicator-position="none"-->
+    <!--        trigger="click"-->
+    <!--      >-->
+    <!--        <el-carousel-item-->
+    <!--          v-for="(image, index) in selectedOffice.images"-->
+    <!--          :key="index"-->
+    <!--          :class="$style.dialogSliderItem">-->
+    <!--          <app-image-->
+    <!--            :class="$style.dialogImg"-->
+    <!--            :x1="image.origin"-->
+    <!--            :webp="image.webp"-->
+    <!--            alt="Office photo"-->
+    <!--            lazy-->
+    <!--          />-->
+    <!--        </el-carousel-item>-->
+    <!--      </el-carousel>-->
+    <!--    </el-dialog>-->
   </div>
 </template>
 
 <script setup>
 import BaseSlider from '@/components/BaseSlider.vue';
+import $style from './OfficeSlider.module.scss';
+
+const sliderOpts = {
+  dots: false,
+  arrows: false,
+  dotsClass: $style.sliderDots,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: true,
+        draggable: true,
+      },
+    },
+  ],
+};
+
+defineProps({
+  office: {
+    type: Array,
+  },
+  currentSlider: {
+    type: Number,
+    default: 0,
+  },
+});
 
 // export default {
 //   name: 'OfficeSlider',
@@ -168,175 +197,4 @@ import BaseSlider from '@/components/BaseSlider.vue';
 // };
 </script>
 
-<style lang="scss" module>
-//@import '../styles/variables';
-@import '@/scss/helpers';
-
-$slider-brake-point: 768px;
-
-.root {
-  max-width: 600px;
-  margin: auto;
-}
-
-.dialog [role='dialog'] {
-  margin: auto 20px;
-  width: 960px;
-}
-
-.dialog [class='el-dialog__body'] {
-  padding: 0;
-}
-
-.office {
-  text-align: center;
-  margin: auto;
-  justify-content: center;
-  display: flex;
-
-  @media (max-width: $slider-brake-point) {
-    flex-direction: column;
-  }
-}
-
-.officePhotos {
-  cursor: pointer;
-  display: inline-block;
-  width: 50%;
-  overflow: unset;
-  margin-right: 20px;
-
-  @media (max-width: $slider-brake-point) {
-    width: 100%;
-  }
-
-  ul li button {
-    width: 8px;
-    height: 8px;
-    padding: 0;
-    border: 0;
-    border-radius: 50%;
-    outline: none;
-    font-size: 0;
-    background-color: var(--dots-color);
-  }
-
-  ul li[class*='is-active'] button {
-    background-color: var(--dots-active-color);
-  }
-}
-
-.dialog {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 0;
-
-  .dialogImg {
-    height: 100%;
-
-    img {
-      object-fit: cover;
-      width: 100%;
-      max-height: 600px;
-      height: 100%;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-
-  .dialogSliderItem {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-}
-
-.carouselImg img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-}
-
-.image {
-  max-width: 330px;
-  margin-right: 20px;
-
-  @media (max-width: $slider-brake-point) {
-    margin: auto;
-  }
-}
-
-.textWrap {
-  text-align: left;
-  max-width: 330px;
-  margin: auto 0;
-  user-select: text;
-
-  @media (max-width: $slider-brake-point) {
-    text-align: center;
-    margin: auto;
-  }
-}
-
-.title {
-  color: var(--blue);
-  font-size: clamp(20px, 3vw, 24px);
-  line-height: 1.25;
-}
-
-.address {
-  opacity: 0.8;
-  color: var(--blue);
-  font-size: 16px;
-  line-height: 1.5;
-}
-
-/*========== Slider ===========*/
-.sliderDots {
-  position: static;
-  margin: 10px auto 0;
-  transform: none;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-}
-/*===== End of Slider =========*/
-
-@media (width > $slider-brake-point) {
-  .dialog {
-    [class='el-carousel__container'] {
-      height: 600px;
-    }
-  }
-}
-
-@media (max-width: $slider-brake-point) {
-  [class='el-dialog'] {
-    width: 60%;
-  }
-
-  .dialogImg {
-    max-height: 600px;
-    height: 100%;
-  }
-}
-
-@media (max-width: 1365px) {
-  .dialog [role='dialog'] {
-    margin: auto 20px;
-    width: 100%;
-  }
-}
-
-@media (max-width: 415px) {
-  .dialog [role='dialog'] {
-    margin: auto 5px;
-  }
-}
-</style>
+<style lang="scss" module></style>
--- src/components/home/HomeMapMark.vue
+++ src/components/home/HomeMapMark.vue
@@ -1,49 +1,16 @@
 <template>
-  <svg
-    v-svg
-    :class="[$style.icon, { [$style.active]: isActive }]"
-    symbol="icon-placemark"
-  >
-    <filter
-      id="filter"
-      x="-6.8%"
-      y="-11.1%"
-      width="113.7%"
-      height="122.2%"
-      filterUnits="objectBoundingBox"
-    >
-      <feGaussianBlur
-        stdDeviation="1"
-        in="SourceGraphic" />
+  <svg :class="[$style.icon, { [$style.active]: isActive }]" symbol="icon-placemark">
+    <filter id="filter" x="-6.8%" y="-11.1%" width="113.7%" height="122.2%" filterUnits="objectBoundingBox">
+      <feGaussianBlur stdDeviation="1" in="SourceGraphic" />
     </filter>
     <defs>
-      <linearGradient
-        id="default"
-        x1="0%"
-        y1="0%"
-        x2="50%"
-        y2="100%"
-      >
-        <stop
-          stop-color="#C40E0E"
-          offset="0%" />
-        <stop
-          stop-color="#F75050"
-          offset="100%" />
+      <linearGradient id="default" x1="0%" y1="0%" x2="50%" y2="100%">
+        <stop stop-color="#C40E0E" offset="0%" />
+        <stop stop-color="#F75050" offset="100%" />
       </linearGradient>
-      <linearGradient
-        id="active"
-        x1="0%"
-        y1="0%"
-        x2="50%"
-        y2="100%"
-      >
-        <stop
-          stop-color="#0058A4"
-          offset="0%" />
-        <stop
-          stop-color="#036CC7"
-          offset="100%" />
+      <linearGradient id="active" x1="0%" y1="0%" x2="50%" y2="100%">
+        <stop stop-color="#0058A4" offset="0%" />
+        <stop stop-color="#036CC7" offset="100%" />
       </linearGradient>
     </defs>
   </svg>
