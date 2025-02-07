798e43e5962a268939545a0413b0f0ff29d364d3
instinct78103 <instinct78103@gmail.com>
2025-02-07 23:29:14 +0500

    Update company page

--- src/pages/company/[[slug]].vue
+++ src/pages/company/[[slug]].vue
@@ -1,10 +1,16 @@
 <script setup>
 import {useRoute} from 'vue-router/auto';
+import Hero from '@/components/Hero.vue';
 
 const route = useRoute()
 </script>
 
 <template>
+
+  <Hero>
+    <strong>Работать с нами</strong> <em>Очень Интересно</em>
+  </Hero>
+
   {{route.params}}
 </template>
 
